ff96b29d4cc70048cfef95b152bc7ba52b958d24
jennyunh <jennyunh@gmail.com>
2022-06-02 22:52:10 -0700

    useEffect and useCallback to run function only once at the beginning

---
+++ build/precache-manifest.f2b248ae908443cde2712c5390aa437d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650ddeedbb34a9ecb1bc4f22c6f219e7",
+    "url": "/StarwarsMovieList/index.html"
+  },
+  {
+    "revision": "0fb32a0d1e0f5110e204",
+    "url": "/StarwarsMovieList/static/css/main.d422f95d.chunk.css"
+  },
+  {
+    "revision": "47d6a7a734720bb82b3f",
+    "url": "/StarwarsMovieList/static/js/2.248da59e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/StarwarsMovieList/static/js/2.248da59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb32a0d1e0f5110e204",
+    "url": "/StarwarsMovieList/static/js/main.73caef8d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/StarwarsMovieList/static/js/main.73caef8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba858d1b430345ca7184",
+    "url": "/StarwarsMovieList/static/js/runtime-main.13a6cadd.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-const [movies, setMovies] = useState([]);
-const [isLoading, setIsLoading] = useState(false);
-const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  
-const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
+    //set isLoading to true
+    setIsLoading(true);
+    setError(null);
 
-//set isLoading to true
-setIsLoading(true);
-setError(null);
-
-  //fetch default is GET request. This function fetches data in the future (async), 
-  //THEN when it gets the data, it will perform a function with the response. 
-  //Response is in JSON format, so the response.json() changes it to regular js.
-  //the response.json() returns another promise.
-  //THEN when the data transformation from json to js is done,
-  /*
+    //fetch default is GET request. This function fetches data in the future (async),
+    //THEN when it gets the data, it will perform a function with the response.
+    //Response is in JSON format, so the response.json() changes it to regular js.
+    //the response.json() returns another promise.
+    //THEN when the data transformation from json to js is done,
+    /*
   fetch('https://swapi.dev/api/films').then(
     response => {return response.json().then(
       data => {console.log(data.results);
@@ -38,61 +36,64 @@ setError(null);
   );
 */
 
-  /*ALTERNATIVE VERSION IS USING ASYNC AND AWAIT: 
+    /*ALTERNATIVE VERSION IS USING ASYNC AND AWAIT: 
   async function fetchMoviesHandler() {
     const response = await fetch('')
     const data = await response.json();
   }
   */
 
-try {
-  const response = await fetch('https://swapi.dev/api/films');
-  if (!response.ok) {
-    throw new Error("SOMETHING WENT WRONG");
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("SOMETHING WENT WRONG");
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseData: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      //error in this case is the string "SOMETHING WENT WRONG"
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+
+
+
+
+  let content = "Found No Movies";
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
 
-  const data = await response.json();
-  const transformedMovies = data.results.map((movieData) => {
-    return {
-      id: movieData.episode_id,
-      title: movieData.title,
-      openingText: movieData.opening_crawl,
-      releaseData: movieData.release_date
-    };
-  });
-  setMovies(transformedMovies);
-}
-//error in this case is the string "SOMETHING WENT WRONG"
-catch (error) {
-  setError(error.message)
-}
-setIsLoading(false); 
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
-}
-
-let content = "Found No Movies";
-
-if (movies.length > 0) {
-  content = <MoviesList movies={movies}/>;
-}
-
-if (error) {
-  content = <p>{error}</p>
-}
-
-if (isLoading) {
-  content = <p>LOADING....</p>
-}
-
+  if (isLoading) {
+    content = <p>LOADING....</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-     {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
