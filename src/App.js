fcc80177f0441f39affa164aefc9d17e1d1ae966
jennyunh <jennyunh@gmail.com>
2022-06-02 22:37:02 -0700

    fetch movies via get request to the starwars API. Handle error

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import './App.css';
 
 function App() {
 const [movies, setMovies] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+const [error, setError] = useState(null);
 
   
-const fetchMoviesHandler = () => {
+const fetchMoviesHandler = async () => {
+
+//set isLoading to true
+setIsLoading(true);
+setError(null);
+
   //fetch default is GET request. This function fetches data in the future (async), 
   //THEN when it gets the data, it will perform a function with the response. 
   //Response is in JSON format, so the response.json() changes it to regular js.
   //the response.json() returns another promise.
   //THEN when the data transformation from json to js is done,
-  //
+  /*
   fetch('https://swapi.dev/api/films').then(
     response => {return response.json().then(
       data => {console.log(data.results);
@@ -25,9 +32,56 @@ const fetchMoviesHandler = () => {
           }
         });
       setMovies(transformedMovies);
+      setIsLoading(false)
       }
     )}
   );
+*/
+
+  /*ALTERNATIVE VERSION IS USING ASYNC AND AWAIT: 
+  async function fetchMoviesHandler() {
+    const response = await fetch('')
+    const data = await response.json();
+  }
+  */
+
+try {
+  const response = await fetch('https://swapi.dev/api/films');
+  if (!response.ok) {
+    throw new Error("SOMETHING WENT WRONG");
+  }
+
+  const data = await response.json();
+  const transformedMovies = data.results.map((movieData) => {
+    return {
+      id: movieData.episode_id,
+      title: movieData.title,
+      openingText: movieData.opening_crawl,
+      releaseData: movieData.release_date
+    };
+  });
+  setMovies(transformedMovies);
+}
+//error in this case is the string "SOMETHING WENT WRONG"
+catch (error) {
+  setError(error.message)
+}
+setIsLoading(false); 
+
+}
+
+let content = "Found No Movies";
+
+if (movies.length > 0) {
+  content = <MoviesList movies={movies}/>;
+}
+
+if (error) {
+  content = <p>{error}</p>
+}
+
+if (isLoading) {
+  content = <p>LOADING....</p>
 }
 
 
@@ -37,7 +91,7 @@ const fetchMoviesHandler = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+     {content}
       </section>
     </React.Fragment>
   );
